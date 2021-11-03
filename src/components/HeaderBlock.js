fff0d2da3bc05cf1cb82db112c6cb8b76168440f
esmith7196 <ericsmith.100@gmail.com>
2021-11-02 20:57:10 -0400

    Remove unused files

---
+++ src/components/HeaderBlock.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HeaderBlock = () => {
+  return (
+    <div className="header-block container" style={{ color: 'white' }}>
+      <div className="columns">
+        <div className="column">
+          <img className="image" src="" alt="" />
+        </div>
+        <div className="column">col 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderBlock;
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import Particle from '../components/Particle';
+import HeaderBlock from '../components/HeaderBlock';
+
+const Index = () => {
+  return (
+    <Layout>
+      <section className="section">
+        <HeaderBlock />
+        <p>hifhsdifhisdfhiosd</p>
+        <Particle></Particle>
+      </section>
+    </Layout>
+  );
+};
+
+export default Index;
